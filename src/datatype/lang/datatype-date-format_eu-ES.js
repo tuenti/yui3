329fb2e5e095b0f7304ebb0c067928cb233bf7fc
Alberto Gragera <alberto@tuenti.com>
2012-07-10 17:58:02 +0200

    Adding all our language files to datatype-date-format module

---
+++ src/datatype/lang/datatype-date-format_eu-ES.js
@@ -0,0 +1,7 @@
+/*
+YUI 3.4.1 (build 4118)
+Copyright 2011 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+YUI.add("lang/datatype-date-format_eu-ES",function(a){a.Intl.add("datatype-date-format","eu-ES",{"a":["iga","asl","asr","asz","osg","osl","lar"],"A":["igandea","astelehena","asteartea","asteazkena","osteguna","ostirala","larunbata"],"b":["urt","ots","mar","api","mai","eka","uzt","abu","ira","urr","aza","abe"],"B":["urtarrila","otsaila","martxoa","apirila","maiatza","ekaina","uztaila","abuztua","iraila","urria","azaroa","abendua"],"c":"%a, %d %b %Y %H:%M:%S %Z","p":["A.M.","P.M."],"P":["a.m.","p.m."],"x":"%d/%m/%y","X":"%H:%M:%S"});},"3.4.1");
---
+++ src/datatype/lang/datatype-date-format_gl-ES.js
@@ -0,0 +1,7 @@
+/*
+YUI 3.4.1 (build 4118)
+Copyright 2011 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+YUI.add("lang/datatype-date-format_gl-ES",function(a){a.Intl.add("datatype-date-format","gl-ES",{"a":["dom","lun","mer","mié","xov","ven","sab"],"A":["domingo","luns","martes","mércores","xoves","venres","sábado"],"b":["xan","feb","mar","abr","mai","xuñ","xul","ago","set","out","nov","dec"],"B":["xaneiro","febreiro","marzo","abril","maio","xuño","xullo","agosto","setembro","outubro","novembro","decembro"],"c":"%a, %d %b %Y %H:%M:%S %Z","p":["A.M.","P.M."],"P":["a.m.","p.m."],"x":"%d/%m/%y","X":"%H:%M:%S"});},"3.4.1");
